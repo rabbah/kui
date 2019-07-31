f28db5f3ac0b05e29de5da91fb8054c72b6d18f9
Joshua Auerbach <joshuaauerbachwatson@gmail.com>
2019-07-31 09:06:54 -0400

    Add --create option to edit command for local file

--- plugins/plugin-editor/src/lib/fetchers.ts
+++ plugins/plugin-editor/src/lib/fetchers.ts
@@ -16,8 +16,8 @@
 
 import * as Debug from 'debug'
 
-import { basename } from 'path'
-import { lstat, readFile } from 'fs'
+import { basename, dirname } from 'path'
+import { lstat, readFile, mkdir, writeFile } from 'fs'
 
 import expandHomeDir from '@kui-shell/core/util/home'
 import { findFile } from '@kui-shell/core/core/find-file'
@@ -91,6 +91,11 @@ export const fetchEntity = async (name: string, parsedOptions, execOptions): Pro
       if (!lastError || (err.code && err.code !== 404)) {
         lastError = err
       }
+      if (parsedOptions.create) {
+          // This seems a little arbitrary but we need to break up hot-potato act between the editor and editor-extensions plugins
+          // In a create situation the user is expecting the local file system fetcher to act alone.  If it fails, that should be it.
+          throw lastError
+      }
     }
   }
 
@@ -100,11 +105,12 @@ export const fetchEntity = async (name: string, parsedOptions, execOptions): Pro
 }
 
 /**
- * Read a local file
+ * Read a local file, optionally creating it
  *
  */
-export const fetchFile: IFetcher = (name: string) => {
+export const fetchFile: IFetcher = (name: string, parsedOptions) => {
   debug('fetching local file', name)
+  const create = parsedOptions && parsedOptions.create
 
   return new Promise<Entity>((resolve, reject) => {
     const filepath = findFile(expandHomeDir(name))
@@ -112,13 +118,21 @@ export const fetchFile: IFetcher = (name: string) => {
     lstat(filepath, (err2: ErrorWithAnyCode, stats) => {
       if (err2) {
         if (err2.code === 'ENOENT') {
-          err2.code = 404
+            debug('file does not exist')
+            if (create) {   
+                debug('creation requested')
+                return resolve(createFile(name))
+            }
+            err2.code = 404
         }
         reject(err2)
+      } else if (create) {
+        reject(new Error(`${name} cannot be created because it already exists`))
       } else if (!stats.isDirectory) {
         const error = new Error('Specified file is a directory')
         reject(error)
       } else {
+        debug("normal read, file exists, create not requested")
         readFile(filepath, (err, data) => {
           if (err) {
             reject(err)
@@ -149,6 +163,41 @@ export const fetchFile: IFetcher = (name: string) => {
       }
     })
   })
+}
+
+// Creates parent directories if needed, then creates a file then edits it
+async function createFile(name: string) {
+    const dir = dirname(name)
+    const base = basename(name)
+    if (base != name) {
+        await makeDirs(dir);
+        return await createAndFetch(name);
+    } else {
+        return createAndFetch(name)
+    }
+}
+
+// Recursively make a directory, ignoring failure
+function makeDirs(dir: string) {
+    return new Promise(function(resolve) {
+         mkdir(dir, { recursive: true }, () => {
+             // Ignore any error on the mkdir
+             resolve(undefined)
+         })
+    })
+}
+
+// Create a file then fetch it
+function createAndFetch(name: string) {
+    return new Promise(function(resolve, reject) {
+        writeFile(name, "", (err) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(undefined)
+            }
+        })
+    }).then(() => fetchFile(name))
 }
 
 /* register the built-in local file fetcher */
